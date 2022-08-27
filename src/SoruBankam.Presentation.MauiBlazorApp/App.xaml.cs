418e7978d127e5d48ff68429b2662cb35b4999de
Ebru Ateş <devatesebru@gmail.com>
2022-08-27 22:55:32 +0300

    Init MauiBlazorApp

---
+++ src/SoruBankam.Presentation.MauiBlazorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoruBankam.Presentation.MauiBlazorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/SoruBankam.Presentation.MauiBlazorApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SoruBankam.Presentation.MauiBlazorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
