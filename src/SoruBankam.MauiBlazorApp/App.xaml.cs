7485956e5f7ac540af4dbb3a3215119da3687214
Ebru Ateş <devatesebru@gmail.com>
2022-09-05 23:23:18 +0300

    program bozuldu yeni maui ekledim

---
+++ src/SoruBankam.MauiBlazorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoruBankam.MauiBlazorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
