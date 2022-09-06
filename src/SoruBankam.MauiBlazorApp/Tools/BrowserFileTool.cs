b235fe99cd2dcdf37e8fe058bb491d9c8d3ac6cb
Ebru Ateş <devatesebru@gmail.com>
2022-09-06 23:21:41 +0300

    Kamera açılma özelliği eklendi.

--- src/SoruBankam.MauiBlazorApp/Tools/BrowserFileTool.cs
+++ src/SoruBankam.MauiBlazorApp/Tools/BrowserFileTool.cs
@@ -15,5 +15,29 @@ namespace SoruBankam.MauiBlazorApp.Tools
             string imageType = imgFile.ContentType;
             return $"data:{imageType};base64,{Convert.ToBase64String(buffers)}";
         }
+
+        public async static Task<String> TakePhotoAndConvertBase64()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                return await ToBase64(photo);
+            }
+
+            throw new Exception("Not Is Capture Supported!");
+        }
+
+        private async static Task<String> ToBase64(FileResult photo)
+        {
+            if (photo == null)
+                throw new Exception("Fotoğraf bulunamadı!");
+
+            using Stream sourceStream = await photo.OpenReadAsync();
+            byte[] buffer = new byte[sourceStream.Length];
+            sourceStream.Read(buffer, 0, buffer.Count());
+
+            return await Task.FromResult($"data:{photo.ContentType};base64,{Convert.ToBase64String(buffer)}");
+        }
     }
 }
